2e7a488741203907b1d2febec46b9fe94281ec8c
Gustavo Carvalho <gustavo.decarvalhogm@gmail.com>
2026-09-23 13:59:09 -0300

    novos exercicios

---
+++ rich/rich001.py
@@ -0,0 +1,7 @@
+from rich import print
+
+print("Olá, [red]Mundo[red]! :earth_americas:")
+print("Olá, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:")
+print(":fire: :vulcan_salute: :alien:")
+
+# Comando para consultar emojis:  python -m rich.emoji
